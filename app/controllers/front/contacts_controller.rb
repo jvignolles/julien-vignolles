23362214cc1fa954ac32177c36aa50eff2e349bc
Julien Vignolles <julien.vignolles@gmail.com>
2015-01-23 21:28:38 +0100

    fix contact form, set menu icon

--- app/controllers/front/contacts_controller.rb
+++ app/controllers/front/contacts_controller.rb
@@ -4,13 +4,14 @@ class Front::ContactsController < Front::BaseController
   end
 
   def new
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new
     @contact.subject = "J’ai une question…" if @contact.subject.blank?
     init_view
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    p = params.require(:contact).permit([:name, :email, :phone, :subject, :description])
+    @contact = Contact.new(p)
     if @contact.save
       Notifier.contact({
         :contact => @contact,
@@ -26,7 +27,7 @@ private
   def init_view
     @editorial = Editorial.find_by_kind(:contact)
     @front_kind = "contacts"
-    @page_heading = "Nous contacter"
+    @page_heading = "Me contacter"
     @page_title = "#{@page_heading} | #{app_name}"
     @breadcrumbs << { key: "contacts", name: "Contact", url: new_front_contact_path }
     @display_quotation = false
