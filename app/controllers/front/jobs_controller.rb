1cd2cd5ca3382276017e82d5a47dafb36bb34e6a
Julien Vignolles <julien.vignolles@gmail.com>
2015-01-27 19:33:09 +0100

    adjust header logo and menu

--- app/controllers/front/jobs_controller.rb
+++ app/controllers/front/jobs_controller.rb
@@ -3,7 +3,7 @@ class Front::JobsController < Front::BaseController
     @jobs = Job.active.ordered.includes(:company)
     @educations = Education.active.ordered
     @front_kind = "jobs"
-    @page_heading = "Expérience"
+    @page_heading = "CV"
     key = "jobs"
     @breadcrumbs << { key: key, name: @page_heading, url: front_jobs_path }
   end
