abc8c7b02f04fe3df6a5524fdee1ce8862ba6005
Julien Vignolles <julien.vignolles@gmail.com>
2015-01-27 18:25:08 +0100

    add projects on job listing

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,6 +14,7 @@ class Job < ActiveRecord::Base
 
   #=== Associations
   belongs_to :company
+  has_many   :projects, -> { order "position" }
   has_and_belongs_to_many :skills, -> { order "position" }
 
   #=== Scopes
